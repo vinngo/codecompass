debd886614d38b46210a64392b0338e11db786de
vinngo <ngovincent9593@gmail.com>
2025-10-19 15:06:42 -0700

    add: navbar context

--- src/components/dashboard/navbar.tsx
+++ src/components/dashboard/navbar.tsx
@@ -1,19 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { NavContext } from "./navcontext";
 
 export function DashboardNavbar() {
   return (
-    <header className="w-full border-b border-border">
-      <div className="container flex h-16 items-center justify-between px-4">
-        <div className="flex items-center gap-2">
-          <div className="h-6 w-6 rounded bg-primary" />
-          <span className="font-semibold text-foreground">
-            Dashboard - Organizations
-          </span>
+    <header className="w-full bg-background border-b border-border">
+      <div className="flex h-13 *:items-center justify-between px-5">
+        <div className="flex items-center flex-row gap-5">
+          {/*Replace this with our logo */}
+          <div className="h-5 w-5 rounded bg-primary" />
+          <div className="font-extralight text-lg text-input">/</div>
+          <NavContext />
         </div>
         <div className="flex items-center gap-4">
-          <Button variant="ghost" size="sm">
-            Home
-          </Button>
           <Button variant="default" size="sm">
             Profile
           </Button>
