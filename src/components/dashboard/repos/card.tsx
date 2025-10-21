3599d6acec8cc904a75ff0277f4bb37ed75d71ac
vinngo <ngovincent9593@gmail.com>
2025-10-21 13:06:12 -0700

    add: repo ui

--- src/components/dashboard/repos/card.tsx
+++ src/components/dashboard/repos/card.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Building2 } from "lucide-react";
-import { OrganizationWithRole } from "@/app/types/supabase";
+import { FolderCode } from "lucide-react";
+import { Repo } from "@/app/types/supabase";
 
-type OrganizationButtonProps = {
-  organization: OrganizationWithRole;
+type RepoButtonProps = {
+  repo: Repo;
 };
 
-export function OrganizationButton({ organization }: OrganizationButtonProps) {
-  const role = organization.user_organizations[0]?.role || "member";
+export function RepoButton({ repo }: RepoButtonProps) {
+  const type = repo.provider;
+  const index_status = repo.index_status;
 
   return (
     <Button
@@ -19,13 +20,13 @@ export function OrganizationButton({ organization }: OrganizationButtonProps) {
       <div className="flex flex-row gap-4 text-sm w-full overflow-hidden">
         {/* Surround in a circular container */}
         <div className="rounded-full bg-black p-2.5 flex-shrink-0">
-          <Building2 className="text-primary-foreground" />
+          <FolderCode className="text-primary-foreground" />
         </div>
         <div className="flex flex-col justify-start items-start min-w-0 flex-1">
-          <span className="font-semibold truncate">{organization.name}</span>
+          <span className="font-semibold truncate">{repo.name}</span>
           <span className="text-muted-foreground text-xs whitespace-nowrap">
-            {role ? "Owner" : "Member"}{" "}
-            <span className="text-md">&middot;</span> 1 project
+            {type} <span className="text-md">&middot;</span>{" "}
+            {repo ? index_status : "not indexed"}
           </span>
         </div>
       </div>
