29309933985e75b2945abb01f9c2bdc0242e36b9
vinngo <ngovincent9593@gmail.com>
2025-11-01 23:03:11 -0700

    chore: add table component

---
+++ src/app/dashboard/org/[id]/team/page.tsx
@@ -0,0 +1,9 @@
+export default function Teams() {
+  return (
+    <div className="mx-auto py-8 px-4">
+      <div className="flex items-center justify-between mb-8">
+        <div className="text-2xl">Team</div>
+      </div>
+    </div>
+  );
+}
