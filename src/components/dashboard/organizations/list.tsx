268e791146233880d888d2b5d5f37fa712bd81b1
stephenwb1 <stephenwb360@gmail.com>
2025-11-04 16:38:39 -0800

    cleaned up empty state for org and repo

--- src/components/dashboard/organizations/list.tsx
+++ src/components/dashboard/organizations/list.tsx
@@ -7,7 +7,7 @@ import { getOrgs } from "@/lib/services/orgService";
 import { Empty } from "@/components/ui/empty";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { Plus } from "lucide-react";
+import { Users, Plus } from "lucide-react";
 
 export function OrganizationList() {
   const { data, isLoading, error } = useQuery<OrganizationWithRole[]>({
@@ -33,10 +33,11 @@ export function OrganizationList() {
 
   if (!data || data.length === 0) {
     return (
-      <div className="flex justify-center items-center scale-150 mt-80">
+      <div className="flex flex-col justify-center items-center scale-150 mt-80">
         <Empty
           title="You Have No Organizations"
           description="Create or join an organization to get started"
+          icon={<Users className="w-6 h-6 text-muted-foreground" />}
         >
           <Button variant="default" size="sm">
             <Link href={`/dashboard/new`}>
