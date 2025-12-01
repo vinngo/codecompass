5ac867963cd13b17e0f2d31732c3d3e8fbce75ad
vinngo <ngovincent9593@gmail.com>
2025-12-01 12:37:10 -0800

    improvement(dashboard): repo and org card visual indicators

--- src/components/dashboard/organizations/card.tsx
+++ src/components/dashboard/organizations/card.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Building2 } from "lucide-react";
+import { Building2, ChevronRight } from "lucide-react";
 import { OrganizationWithRole } from "@/app/types/supabase";
 import { useRouter } from "next/navigation";
 import { usePrefetchRoute } from "@/hooks/usePrefetchRoute";
+import { motion } from "framer-motion";
 
 type OrganizationButtonProps = {
   organization: OrganizationWithRole;
@@ -16,25 +17,33 @@ export function OrganizationButton({ organization }: OrganizationButtonProps) {
   const { prefetchOrgPage } = usePrefetchRoute();
 
   return (
-    <Button
-      variant="outline"
-      className="bg-background h-auto min-h-21 justify-start p-4 w-full hover:bg-elevated"
-      onClick={() => router.push(`/dashboard/org/${organization.id}`)}
-      onMouseEnter={() => prefetchOrgPage(organization.id)}
-    >
-      <div className="flex flex-row gap-4 text-sm w-full overflow-hidden">
-        {/* Surround in a circular container */}
-        <div className="rounded-full dark:bg-black border-2 border-primary p-2.5 shrink-0">
-          <Building2 className="text-primary-foreground" />
+    <motion.div whileHover="hover" className="parent">
+      <Button
+        variant="outline"
+        className="bg-background h-auto min-h-21 justify-start p-4 w-full hover:bg-elevated"
+        onClick={() => router.push(`/dashboard/org/${organization.id}`)}
+        onMouseEnter={() => prefetchOrgPage(organization.id)}
+      >
+        <div className="flex flex-row gap-4 text-sm w-full overflow-hidden">
+          {/* Surround in a circular container */}
+          <div className="rounded-full dark:bg-black border-2 border-primary p-2.5 shrink-0">
+            <Building2 className="text-primary-foreground" />
+          </div>
+          <div className="flex flex-col justify-start items-start min-w-0 flex-1">
+            <span className="font-semibold truncate">{organization.name}</span>
+            <span className="text-muted-foreground text-xs whitespace-nowrap">
+              {role ? "Owner" : "Member"}{" "}
+              <span className="text-md">&middot;</span> 1 project
+            </span>
+          </div>
+          <motion.div
+            initial={{ opacity: 0.5 }}
+            variants={{ hover: { x: 4, opacity: 1 } }}
+          >
+            <ChevronRight className="text-primary-foreground" />
+          </motion.div>
         </div>
-        <div className="flex flex-col justify-start items-start min-w-0 flex-1">
-          <span className="font-semibold truncate">{organization.name}</span>
-          <span className="text-muted-foreground text-xs whitespace-nowrap">
-            {role ? "Owner" : "Member"}{" "}
-            <span className="text-md">&middot;</span> 1 project
-          </span>
-        </div>
-      </div>
-    </Button>
+      </Button>
+    </motion.div>
   );
 }
--- src/components/dashboard/repos/card.tsx
+++ src/components/dashboard/repos/card.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { FolderCode } from "lucide-react";
+import { FolderCode, ChevronRight } from "lucide-react";
 import { Github } from "@geist-ui/icons";
 import { Gitlab } from "@geist-ui/icons";
 import { Repo } from "@/app/types/supabase";
 import { useRouter } from "next/navigation";
+import { usePrefetchRoute } from "@/hooks/usePrefetchRoute";
+import { motion } from "framer-motion";
 
 type RepoButtonProps = {
   repo: Repo;
@@ -15,43 +17,47 @@ export function RepoButton({ repo }: RepoButtonProps) {
   const router = useRouter();
   const type = repo.provider;
   const index_status = repo.index_status;
-
-  const handlePrefetch = () => {
-    // Prefetch repo page route
-    router.prefetch(`/dashboard/repo/${repo.id}`);
-  };
+  const { prefetchRepoPage } = usePrefetchRoute();
 
   return (
-    <Button
-      variant="outline"
-      className="bg-background h-auto min-h-21 justify-start p-4 w-full hover:bg-elevated"
-      onClick={() => router.push(`/dashboard/repo/${repo.id}`)}
-      onMouseEnter={handlePrefetch}
-    >
-      <div className="flex flex-row gap-4 text-sm w-full overflow-hidden">
-        {/* Surround in a circular container */}
-        <div className="rounded-full dark:bg-black border-2 border-primary p-2.5 shrink-0">
-          {type === "github" ? (
-            <Github className="w-4 h-4" />
-          ) : type === "gitlab" ? (
-            <Gitlab className="w-4 h-4" />
-          ) : (
-            <FolderCode className="w-4 h-4 text-white" />
-          )}
+    <motion.div whileHover="hover" className="parent">
+      <Button
+        variant="outline"
+        className="bg-background h-auto min-h-21 justify-start p-4 w-full hover:bg-elevated"
+        onClick={() => router.push(`/dashboard/repo/${repo.id}`)}
+        onMouseEnter={() => prefetchRepoPage(repo.id)}
+      >
+        <div className="flex flex-row gap-4 text-sm w-full overflow-hidden">
+          {/* Surround in a circular container */}
+          <div className="rounded-full dark:bg-black border-2 border-primary p-2.5 shrink-0">
+            {type === "github" ? (
+              <Github className="w-4 h-4" />
+            ) : type === "gitlab" ? (
+              <Gitlab className="w-4 h-4" />
+            ) : (
+              <FolderCode className="w-4 h-4 text-white" />
+            )}
+          </div>
+          <div className="flex flex-col justify-start items-start min-w-0 flex-1">
+            <span className="font-semibold truncate">{repo.name}</span>
+            <span className="text-muted-foreground text-xs whitespace-nowrap">
+              {type === "github"
+                ? "GitHub"
+                : type === "gitlab"
+                  ? "GitLab"
+                  : "Local"}{" "}
+              <span className="text-md">&middot;</span>{" "}
+              {index_status || "Not Indexed"}
+            </span>
+          </div>
+          <motion.div
+            initial={{ opacity: 0.5 }}
+            variants={{ hover: { x: 4, opacity: 1 } }}
+          >
+            <ChevronRight className="text-primary-foreground" />
+          </motion.div>
         </div>
-        <div className="flex flex-col justify-start items-start min-w-0 flex-1">
-          <span className="font-semibold truncate">{repo.name}</span>
-          <span className="text-muted-foreground text-xs whitespace-nowrap">
-            {type === "github"
-              ? "GitHub"
-              : type === "gitlab"
-                ? "GitLab"
-                : "Local"}{" "}
-            <span className="text-md">&middot;</span>{" "}
-            {index_status || "Not Indexed"}
-          </span>
-        </div>
-      </div>
-    </Button>
+      </Button>
+    </motion.div>
   );
 }
